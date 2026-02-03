0e9b8a1dfb08f91ed77710f0fe60540eee8a2405
Amr Mohammed <amr.mohamed2453@gmail.com>
2026-02-03 20:46:46 +0200

    added 3 levels
    added 3 buttons to choose level with 3 grids
    first 3*2
    second 3*3
    third 3*4

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,52 +1,125 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using PrimeTween;
 
 public class Controller : MonoBehaviour
 {
+    [Header("Game")]
     [SerializeField] Card cardPrefab;
     [SerializeField] Transform gridTransform;
+    [SerializeField] GridLayoutGroup gridLayout;
     [SerializeField] Sprite[] sprites;
 
+    [Header("UI")]
+    [SerializeField] GameObject btn3x2;
+    [SerializeField] GameObject btn3x3;
+    [SerializeField] GameObject btn3x4;
+    [SerializeField] GameObject backButton;
+
+    [Header("Audio")]
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip matchSound;
     [SerializeField] AudioClip mismatchSound;
     [SerializeField] AudioClip finishSound;
+    [SerializeField] AudioClip buttonSound;
 
     private List<Sprite> spritePairs;
-
     Card firstSelected;
     Card secondSelected;
-
     int matchCounts;
 
     private void Start()
     {
-        PrepareSprites();
+        gridTransform.gameObject.SetActive(false);
+        backButton.SetActive(false);
+    }
+
+    // Methods without parameters for OnClick
+    public void StartGame3x2() { StartGame(3, 2); }
+    public void StartGame3x3() { StartGame(3, 3); }
+    public void StartGame3x4() { StartGame(3, 4); }
+
+    void StartGame(int columns, int rows)
+    {
+        audioSource.PlayOneShot(buttonSound);
+
+        btn3x2.SetActive(false);
+        btn3x3.SetActive(false);
+        btn3x4.SetActive(false);
+
+        gridTransform.gameObject.SetActive(true);
+        backButton.SetActive(true);
+
+        SetupGrid(columns);
+        PrepareSprites(columns * rows);
         CreateCards();
+    }
+
+    void SetupGrid(int columns)
+    {
+        gridLayout.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        gridLayout.constraintCount = columns;
+    }
+
+    void PrepareSprites(int totalCards)
+    {
+        spritePairs = new List<Sprite>();
+        int pairsCount = totalCards / 2;
+
+        for (int i = 0; i < pairsCount; i++)
+        {
+            Sprite s = sprites[i % sprites.Length];
+            spritePairs.Add(s);
+            spritePairs.Add(s);
+        }
+
+        if (totalCards % 2 != 0)
+            spritePairs.Add(sprites[0]);
+
+        Shuffle(spritePairs);
+    }
+
+    void CreateCards()
+    {
+        foreach (Transform child in gridTransform)
+            Destroy(child.gameObject);
+
+        for (int i = 0; i < spritePairs.Count; i++)
+        {
+            Card card = Instantiate(cardPrefab, gridTransform);
+            card.setIconSprite(spritePairs[i]);
+            card.controller = this;
+        }
+    }
+
+    void Shuffle(List<Sprite> list)
+    {
+        for (int i = list.Count - 1; i > 0; i--)
+        {
+            int r = Random.Range(0, i + 1);
+            (list[i], list[r]) = (list[r], list[i]);
+        }
     }
 
     public void setSelected(Card card)
     {
-        if (!card.isSelected)
+        if (card.isSelected) return;
+
+        card.Show();
+
+        if (firstSelected == null)
         {
-            card.Show();
-
-            if (firstSelected == null)
-            {
-                firstSelected = card;
-                return;
-            }
-
-            if (secondSelected == null)
-            {
-                secondSelected = card;
-                StartCoroutine(CheckMatching(firstSelected, secondSelected));
-                firstSelected = null;
-                secondSelected = null;
-            }
+            firstSelected = card;
+            return;
         }
+
+        secondSelected = card;
+        StartCoroutine(CheckMatching(firstSelected, secondSelected));
+
+        firstSelected = null;
+        secondSelected = null;
     }
 
     IEnumerator CheckMatching(Card a, Card b)
@@ -63,8 +136,8 @@ public class Controller : MonoBehaviour
                 audioSource.PlayOneShot(finishSound);
 
                 PrimeTween.Sequence.Create()
-                    .Chain(PrimeTween.Tween.Scale(gridTransform, Vector3.one * 1.2f, 0.2f, ease: PrimeTween.Ease.OutBack))
-                    .Chain(PrimeTween.Tween.Scale(gridTransform, Vector3.one, 0.1f));
+                    .Chain(Tween.Scale(gridTransform, Vector3.one * 1.2f, 0.2f))
+                    .Chain(Tween.Scale(gridTransform, Vector3.one, 0.1f));
             }
         }
         else
@@ -75,35 +148,27 @@ public class Controller : MonoBehaviour
         }
     }
 
-    private void PrepareSprites()
+    public void Back()
     {
-        spritePairs = new List<Sprite>();
-        for (int i = 0; i < sprites.Length; i++)
-        {
-            spritePairs.Add(sprites[i]);
-            spritePairs.Add(sprites[i]);
-        }
-        shuffleSprites(spritePairs);
+        audioSource.PlayOneShot(buttonSound);
+
+        ClearGrid();
+
+        gridTransform.gameObject.SetActive(false);
+        backButton.SetActive(false);
+
+        btn3x2.SetActive(true);
+        btn3x3.SetActive(true);
+        btn3x4.SetActive(true);
     }
 
-    void CreateCards()
+    void ClearGrid()
     {
-        for (int i = 0; i < spritePairs.Count; i++)
-        {
-            Card card = Instantiate(cardPrefab, gridTransform);
-            card.setIconSprite(spritePairs[i]);
-            card.controller = this;
-        }
-    }
+        foreach (Transform child in gridTransform)
+            Destroy(child.gameObject);
 
-    void shuffleSprites(List<Sprite> spriteList)
-    {
-        for (int i = spriteList.Count - 1; i > 0; i--)
-        {
-            int randomIndex = Random.Range(0, i + 1);
-            Sprite temp = spriteList[i];
-            spriteList[i] = spriteList[randomIndex];
-            spriteList[randomIndex] = temp;
-        }
+        matchCounts = 0;
+        firstSelected = null;
+        secondSelected = null;
     }
 }
